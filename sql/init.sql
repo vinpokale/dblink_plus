f0da12bc96c27077ffe5e02a44c816702401c057
bwtakacy <bwtakacy@gmail.com>
2015-07-02 12:58:14 +0900

    Move HTML files to top and remove other files.

--- sql/init.sql
+++
@@ -1,6 +0,0 @@
-SET client_min_messages = warning;
-\set ECHO none
-\i dblink_plus.sql
-\set ECHO all
-RESET client_min_messages;
-\! sh regress_init.sh
--- uninstall_dblink_plus.sql
+++
@@ -1,3 +0,0 @@
-SET search_path = public;
-
-DROP SCHEMA dblink CASCADE;
